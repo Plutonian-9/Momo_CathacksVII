a8994f335fd1bf94408345fadfab8c5478fb1c5d
PastelKobold <103293322+PastelKobold@users.noreply.github.com>
2022-04-09 17:03:57 -0400

    Working button to scroll through text

--- MainFrame.java
+++ MainFrame.java
@@ -9,8 +9,12 @@ import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 import javax.swing.plaf.DimensionUIResource;
 
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.util.EventListener;
 
 public class MainFrame extends JFrame {
  
@@ -20,6 +24,10 @@ public class MainFrame extends JFrame {
 
   private JPanel gameWindow = new JPanel();
   private JPanel textPanel = new JPanel();
+  private String text[] = new String[60];
+  private JLabel text_label = new JLabel("Hello");
+  private int text_counter = 1;
+  private JButton button = new JButton("Button 1");
   
   public MainFrame() {
 
@@ -33,17 +41,40 @@ public class MainFrame extends JFrame {
     textPanel.setBackground(Color.LIGHT_GRAY);
     textPanel.setBorder(new EmptyBorder(10,10,10,10));
     textPanel.setPreferredSize(new Dimension(800,250));
-
+    text_label.setForeground(Color.BLACK);
+    text_label.setOpaque(true);
+    text_label.setBackground(Color.LIGHT_GRAY);
+    
+    //-----------------------Button------------------------------
+    button.setMinimumSize(new Dimension(400, 200));
+    button.addActionListener(new Clicklistener());
 
     ////----------------------Main Window------------------------
     this.setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
     this.add(gameWindow);
     this.add(textPanel);
+    textPanel.add(text_label);
+    textPanel.add(button);
     this.setTitle("MoMo");
     this.setSize(800, 800);
     this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     this.setVisible(true);
+
+    //--------------------Game Text Responses---------------------
+    text[0] = "Welcome!";
+    text[1] = "This is Mariam";
+    text[2] = "Mariam works at the library and is currently on her way to the library";
+    text[3] = ">I'm just testing";
+    
   }
-  
-  
+
+  private class Clicklistener implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e){
+        text_label.setText(text[text_counter]);
+        text_counter++;
+    }
+  }
 }
+
