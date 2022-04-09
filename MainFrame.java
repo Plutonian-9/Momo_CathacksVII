47d4a90a049b7c6ffdd95a49ffe95b3ede1fc66c
Sydney Chapman <58576489+Plutonian-9@users.noreply.github.com>
2022-04-08 20:36:57 -0400

    Create basic file

---
+++ MainFrame.java
@@ -0,0 +1,25 @@
+/*
+Project:
+Contributers: 
+
+*/
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainFrame extends JFrame {
+ 
+  public static void main(String[] args) {
+    MainFrame window = new MainFrame();
+  }
+  
+  public MainFrame() {
+      this.setTitle("MoMo");
+      this.setSize(1150, 530);
+      this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+      this.setVisible(true);
+  }
+  
+  
+}
