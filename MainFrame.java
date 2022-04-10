61c6ccf7bc49299eaa36a4f9e7aa06f5266c30ae
PastelKobold <103293322+PastelKobold@users.noreply.github.com>
2022-04-10 09:17:52 -0400

    Added some of my changes to main branch. Plus buttons are set up

--- MainFrame.java
+++ MainFrame.java
@@ -60,9 +60,406 @@ public class MainFrame extends JFrame {
 
 
 
+<<<<<<< Updated upstream
   public void fillPlotPoints() {
     // title screen
     plotPoints[0] = new StoryFrame("library.png", "Marian the Librarian: A Choose-Your-Own Adventure Game", "", {}, 0, 2, {}, 0, 1);
+=======
+    @Override
+    public void actionPerformed(ActionEvent e){
+      while(endOfPath != 1){
+        if(e.getSource() == button){
+            switch(case_story){
+            case 0:
+            text_label.setText(text[case_story]);
+            case_story = 1;
+            break;
+            case 1:
+            text_label.setText(text[case_story]);
+            case_story = 2;
+            break;
+            case 2:
+            text_label.setText(text[case_story]);
+            case_story = 3;
+            break;
+            case 3:
+            text_label.setText(text[case_story]);
+            //2 buttons
+            button.setVisible(false);
+            buttonY.setVisible(true);
+            buttonN.setVisible(true);
+            //if No (button 2)case_story = 4; if (button 3) yes case_story = 21;
+            break;
+            case 4:
+            buttonY.setVisible(false);
+            buttonN.setVisible(false);
+            text_label.setText(text[case_story]);
+            buttonA.setVisible(true);
+            buttonB.setVisible(true);
+            //if B case_story = 5; if A case_story = 22;
+            break;
+            case 5:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 6;
+            break;
+            case 6:
+            text_label.setText(text[case_story]);
+            case_story = 7;
+            break;
+            case 7:
+            text_label.setText(text[case_story]);
+            case_story = 8;
+            break;
+            case 8:
+            text_label.setText(text[case_story]);
+            case_story = 9;
+            break;
+            case 9:
+            button.setVisible(false);
+            buttonY.setVisible(true);
+            buttonN.setVisible(true);
+            text_label.setText(text[case_story]);
+            //if yes case_story=24 if no case_story = 10;
+            break;
+            case 10:
+            buttonY.setVisible(false);
+            buttonN.setVisible(false);
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 11;
+            break;
+            case 11:
+            button.setVisible(false);
+            buttonA.setVisible(true);
+            buttonB.setVisible(true);
+            buttonC.setVisible(true);
+            buttonD.setVisible(true);
+            text_label.setText(text[case_story]);
+            //11 if A||B case_story = 25; if C case_story = 26; if D case_story = 12;
+            break;
+            case 12:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            buttonC.setVisible(false);
+            buttonD.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 13;
+            break;
+            case 13:
+            text_label.setText(text[case_story]);
+            case_story = 14;
+            break;
+            case 14:
+            text_label.setText(text[case_story]);
+            case_story = 15;
+            break;
+            case 15:
+            button.setVisible(false);
+            buttonA.setVisible(true);
+            buttonB.setVisible(true);
+            text_label.setText(text[case_story]);
+            // 15 if A; if B case_story = 16; - NEEDS TO BE CHANGED
+            break;
+            case 16:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 17;
+            break;
+            case 17:
+            text_label.setText(text[case_story]);
+            case_story = 18;
+            break;
+            case 18:
+            text_label.setText(text[case_story]);
+            case_story = 19;
+            break;
+            case 19:
+            text_label.setText(text[case_story]);
+            case_story = 20;
+            break;
+            case 20:
+            text_label.setText(text[case_story]);
+            endOfPath = 1;
+            //No path yet
+            break;
+            case 21:
+            button.setVisible(true);
+            buttonY.setVisible(false);
+            buttonN.setVisible(false);
+            text_label.setText(text[case_story]);
+            //case_story = 28;
+            break;
+            case 22:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 35;
+            break;
+            case 23:
+            text_label.setText(text[case_story]);
+            //no path yet. DOESN'T EXIST
+            break;
+            case 24:
+            buttonY.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 10;
+            break;
+            case 25:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            buttonC.setVisible(false);
+            buttonD.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            //nothing yet
+            break;
+            case 26:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            buttonC.setVisible(false);
+            buttonD.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            //nothing yet
+            break;
+            case 28:
+            text_label.setText(text[case_story]);
+            case_story = 4;
+            break;
+            case 29:
+            button.setVisible(false);
+            buttonA.setVisible(true);
+            buttonB.setVisible(true);
+            text_label.setText(text[case_story]);
+            //29 if A case_story = 30; if B case_story = 10;
+            break;
+            case 30:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 32;
+            break;
+            case 32:
+            button.setVisible(false);
+            buttonA.setVisible(true);
+            buttonB.setVisible(true);
+            text_label.setText(text[case_story]);
+            //32 if A case_story = 11; if B case_story = 34; UPDATE A TO 11
+            break;
+            case 34:
+            button.setVisible(false);
+            buttonC.setVisible(true);
+            buttonD.setVisible(true);
+            text_label.setText(text[case_story]);
+            //BUTTONS A,B,C,D ALL NEED TO BE ADDED.
+            break;
+            case 35:
+            text_label.setText(text[case_story]);
+            case_story = 29;
+            break;
+            case 36:
+            buttonA.setVisible(false);
+            buttonB.setVisible(false);
+            buttonC.setVisible(false);
+            buttonD.setVisible(false);
+            buttonY.setVisible(true);
+            buttonN.setVisible(true);
+            text_label.setText(text[case_story]);
+            //36 if Yes case_story = 37; if no case_story = 38;
+            break;
+            case 37:
+            buttonY.setVisible(false);
+            buttonN.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            break;
+            case 38:
+            buttonY.setVisible(false);
+            buttonN.setVisible(false);
+            button.setVisible(true);
+            text_label.setText(text[case_story]);
+            case_story = 39;
+            break;
+            case 39:
+            button.setVisible(false);
+            buttonA.setVisible(true);
+            buttonB.setVisible(true);
+            buttonC.setVisible(true);
+            buttonD.setVisible(true);
+            text_label.setText(text[case_story]);
+            // 39 if A||B case_story = 40; if C case_story = 41; if D case_story = 42;
+            break;
+            case 40:
+            text_label.setText(text[case_story]);
+            case_story = 43;
+            break;
+            case 41:
+            text_label.setText(text[case_story]);
+            case_story = 43;
+            break;
+            case 42:
+            text_label.setText(text[case_story]);
+            case_story = 43;
+            break;
+            case 43:
+            //CHANGED TO JUST 45
+            text_label.setText(text[case_story]);
+            //if A case_story = 44; if B case_story = 45;
+            break;
+            case 44:
+            //DOESNT EXIST
+            text_label.setText(text[case_story]);
+            endOfPath = 1;
+            break;
+            case 45: 
+            text_label.setText(text[case_story]);
+            case_story = 46;
+            break;
+            case 46:
+            text_label.setText(text[case_story]);
+            //46 if A||D case_story =47; if B case_story = 48; if C case_story = 49;
+            break;
+            case 47:
+            text_label.setText(text[case_story]);
+            endOfPath = 1;
+            break;
+            case 48:
+            text_label.setText(text[case_story]);
+            endOfPath = 1;
+            break;
+            case 49:
+            text_label.setText(text[case_story]);
+            endOfPath = 1;
+            break;
+            case 50:
+            //currently unused
+            text_label.setText(text[case_story]);
+            endOfPath = 1;
+            break;
+          }
+        }
+      }
+        if(e.getSource() == buttonY){
+          if(case_story == 3){
+            case_story = 4;
+          }
+          if(case_story == 9){
+            case_story = 24;
+          }
+          if(case_story == 36){
+            case_story = 37;
+          }
+        }
+        if(e.getSource() == buttonN){
+          if(case_story == 3){
+            case_story = 21;
+          }
+          if(case_story == 9){
+            case_story = 24;
+          }
+          if(case_story == 36){
+            case_story = 38;
+          }
+        }
+        if(e.getSource() == buttonA){
+          if(case_story == 4){
+            case_story = 22;
+          }
+          if(case_story == 11){
+            case_story = 25;
+          }
+          if(case_story == 15){
+            case_story = 16;
+          }
+          if(case_story == 29){
+            case_story = 30;
+          }
+          if(case_story == 32){
+            case_story = 11;
+          }
+          if(case_story == 34){
+            case_story = 36;
+          }
+          if(case_story == 39){
+            case_story = 40;
+          }
+          if(case_story == 43){
+            case_story = 44;
+          }
+          if(case_story == 46){
+            case_story = 47;
+          }
+        }
+        if(e.getSource() == buttonB){
+          if(case_story == 4){
+            case_story = 5;
+          }
+          if(case_story == 11){
+            case_story = 25;
+          }
+          if(case_story == 15){
+            case_story = 16;
+          }
+          if(case_story == 29){
+            case_story = 10;
+          }
+          if(case_story == 33){
+            case_story = 34;
+          }
+          if(case_story == 34){
+            case_story = 36;
+          }
+          if(case_story == 39){
+            case_story = 40;
+          }
+          if(case_story == 43){
+            case_story = 45;
+          }
+          if(case_story == 46){
+            case_story = 48;
+          }
+        }
+        if(e.getSource() == buttonC){
+          if(case_story == 11){
+            case_story = 26;
+          }
+          if(case_story == 34){
+            case_story = 36;
+          }
+          if(case_story == 39){
+            case_story = 41;
+          }
+          if(case_story == 46){
+            case_story = 49;
+          }
+        }
+        if(e.getSource() == buttonD){
+          if(case_story == 11){
+            case_story = 12;
+          }
+          if(case_story == 34){
+            case_story = 36;
+          }
+          if(case_story == 39){
+            case_story = 42;
+          }
+          if(case_story == 46){
+            case_story = 47;
+          }
+        }
+    }
+>>>>>>> Stashed changes
   }
 
 }
